89414bc74912fba931bd173b5edf34ec93b3f0b1
Cliff Hults III <ch3@radnet.com>
2018-07-26 16:16:53 -0400

    Initial Commit

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A library to interact with Netwrix Search API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/BongoEADGC6/netwrix_api",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
