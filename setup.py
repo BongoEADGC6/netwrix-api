158eceec1fdb983522c3a2ed3290822ca57fe527
Cliff Hults III <ch3@radnet.com>
2018-07-26 17:13:37 -0400

    Reorganized

--- setup.py
+++ setup.py
@@ -4,18 +4,24 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="netwrix_api",
-    version="0.0.2",
+    name="netwrix-api",
+    version="0.0.3",
     author="Cliff Hults",
     author_email="bongoeadgc6@gmail.com",
     description="A library to interact with Netwrix Search API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/BongoEADGC6/netwrix_api",
+    url="https://github.com/BongoEADGC6/netwrix-api",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
+    install_requires=[
+        "sys",
+        "requests",
+        "json",
+        "logging"
+    ]
 )
